13dfc8fccbdde475c894880bd7d98df9b3065f03
phegorez <metha.nompd@gmail.com>
2020-10-20 21:44:54 +0700

    master

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hello/app_sign_in/sign_in.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignInPage(),
+    );
+  }
+}
